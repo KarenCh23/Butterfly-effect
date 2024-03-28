00e415deab5cbdf6f485898af3be74bcb5a128c3
KarenCh23 <karenchamand@hotmail.fr>
2024-03-28 14:32:41 +0100

    fix butterfly logo error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import logo from "../public/butterfly_logo.png";
+import logo from "./butterfly_logo.png";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Canvas } from "@react-three/fiber";
